2ec719402f0b646387456e0946ae1f3a48a0370f
rim051488 <sin.naga71@outlook.jp>
2022-06-15 15:53:00 +0900

    更新

--- Ex05/Ex0501.java
+++ Ex05/Ex0501.java
@@ -1,5 +1,12 @@
 public class Ex0404{
     public static void main(String[] args){
+    	Player1 player[] = new Player1[6];
+    	String name[] = {"佐藤","鈴木","高橋","田中","伊藤","渡辺"};
+    	int height[] = {1400,150,182,177,165,159};
+    	int weight[] = {39,45,96,78,69,560};
+    	for (int i = 0; i < player.length;i++){
+    		player[i] = new Player1(name[i],height[i],weight[i]);
+    	}
         Profile.Number();
     	Profile.Name();
     }
@@ -24,9 +31,20 @@ class Player1{
 		secretBalloon();
 	}
 	public void calcBmi(){
-		
+		double m = height / 100;
+		bmi = (weight / (m * m));
 	}
 	public void print(){
+		System.out.println(name+"さんの身長は"+height+"cm、体重は"+weight+"Kg、BMIは"+bmi+"です。");
+		if (bmiHeavy < bmi){
+			System.out.println(name+"さんは太りすぎです。");
+		}
+		if (bmiLight > bmi){
+			System.out.println(name+"さんは瘦せすぎです。");
+		}
+		else{
+			System.out.println(name+"さんは普通です。");
+		}
 	}
 	private void secretBoots(){
 		if (name.equals("鈴木")){
