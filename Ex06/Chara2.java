a129d18f4990e8ad1e4018d97bceff9f7b0350c7
rim051488 <sin.naga71@outlook.jp>
2022-07-13 09:55:12 +0900

    a

---
+++ Ex06/Chara2.java
@@ -0,0 +1,19 @@
+public class Chara1 extends Chara0{
+	@Override
+	public String getName(){
+		String str = "私の名前は"+super.getName() +"です。" ;
+		return str;
+	}
+	public String getAge(){
+		String str = "私の年齢は"+super.getAge() +"歳です" ;
+		return str;
+	}
+	public String getRace(){
+		String str ="私の種族は"+super.getRace() +"です。" ;
+		return str;
+	}
+	public String getJob(){
+		String str = "私の職業は"+super.getJob() +"です。" ;
+		return str;
+	}
+}
