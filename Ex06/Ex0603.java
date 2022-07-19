107f68bc9d7d6473da95c4040011c64c5a0a443a
rim051488 <sin.naga71@outlook.jp>
2022-07-19 11:12:06 +0900

    a

--- Ex06/Ex0603.java
+++ Ex06/Ex0603.java
@@ -3,20 +3,40 @@ public class Ex0603{
 	public static void main(String[] args){
 		MyBuddy buddy = new MyBuddy();
 		int enemyHP = 100;
+		int playerHP = 100;
 		int finishtrun = 0;
 		System.out.println(buddy.getGreeting());
 		System.out.println("あなたの名前を入力してください");
 		Scanner scanner = new Scanner (System.in);
 		String name = scanner.nextLine();
 		System.out.println(name+"です。よろしく");
-		while (enemyHP > 0){
-			System.out.println("攻撃"+(++finishtrun)+"回目");
+		System.out.println(buddy.getTraining(name));
+		while (enemyHP > 0 && playerHP > 0){
+			System.out.println(name + "の攻撃"+(++finishtrun)+"回目");
 			if ((int)(Math.random() * 5) ==0) {
-				enemyHP -= buddy.finish();
+				enemyHP -= buddy.PlayerFinish();
 			}else{
-				enemyHP -= buddy.attack();
+				enemyHP -= buddy.PlayerAttack();
 			}
-			System.out.println("敵の体力：残り"+enemyHP+"HP");
+			System.out.println(buddy.getName() + "の体力：残り"+enemyHP+"HP");
+			System.out.println();
+			if (enemyHP > 0){
+				System.out.println(buddy.getName()+"の攻撃"+(finishtrun)+"回目");
+				if ((int)(Math.random() * 10) ==0) {
+					playerHP -= buddy.finish();
+				}else{
+					playerHP -= buddy.attack();
+				}
+				System.out.println(name + "の体力：残り"+playerHP+"HP");
+				System.out.println();
+			}
+			if (playerHP <= 0){
+				System.out.println(buddy.getFailure());
+				System.out.println("わかりました\nまた力をつけて挑戦します");
+			}else if(enemyHP <= 0){
+				System.out.println(buddy.getSuccess());
+				System.out.println("はい、魔王を倒してきます");
+			}
 		}
     	Profile.Number();
     	Profile.Name();		
--- Ex06/MyBuddy.java
+++ Ex06/MyBuddy.java
@@ -3,20 +3,81 @@ public class MyBuddy extends AbstractBuddy{
 		int number = (int)(Math.random()*DefBuddyCnt);
 		setBuddyNumber(number);
 	}
+	public String getTraining(String name){
+		int number = getBuddyNumber();
+		switch(number){
+		case 0:
+			return name + "、あんたの訓練をはじめよう\n俺を倒したら訓練終了だ。";
+		case 1:
+			return name + "、お前さんの訓練をはじめよう\nワシを倒したら訓練終了じゃ。";
+		case 2:
+			return name + "様、あなた様の訓練をはじめましょう\nわたくしを倒したら訓練終了ですわ。";
+		case 3:
+			return name + "君、君の訓練を始めよう\n僕を倒したら訓練終了だよ。";
+		case 4:
+			return name + "、あなたの訓練をはじめましょう\n私を倒したら訓練終了です。";
+		default:
+			return name + "訓練をはじめる\n…";
+		}
+	}
+		public String getSuccess(){
+		int number = getBuddyNumber();
+		switch(number){
+		case 0:
+			return "これだけ動けるのなら心配はないな\n頑張って魔王を倒してこい！";
+		case 1:
+			return "お前さんの勝ちじゃ\nお前さんなら魔王に勝てるかもしれん。頑張ってくるのじゃ！";
+		case 2:
+			return "あなた様なら魔王を倒せると思うのですわ\n陰ながら応援していますわ";
+		case 3:
+			return "この僕を倒したから魔王を倒せると思うよ\n頑張ってきてね";
+		case 4:
+			return "これだけできるのなら心配ないですね\nその力で魔王を倒してください";
+		default:
+			return"…魔王を倒してこい";
+		}
+	}
+	public String getFailure(){
+		int number = getBuddyNumber();
+		switch(number){
+		case 0:
+			return "これでは魔王には勝てん\n修行しなおしだ";
+		case 1:
+			return "この老いぼれを倒せないなら魔王は倒せないのじゃ\n修行してこいなのじゃ";
+		case 2:
+			return "力不足ですわ\nもっと力をつけてくるのですわ";
+		case 3:
+			return "これじゃあ魔王は倒せないよ\n修行しなおしですね";
+		case 4:
+			return "私を倒せないなら魔王は倒せないよ\nもっと力をつけてきてね";
+		default:
+			return "力不足だ";
+		}
+	}
+	public int PlayerAttack(){
+		int damage = (int)(Math.random() *10+10);
+		System.out.println("いきます！");
+		return damage;
+	}
+		public int PlayerFinish(){
+		int damage = (int)(Math.random() * 50+50);
+		System.out.println("真・溜め斬り！");
+		return damage;
+	}
 	@Override
 	public String getGreeting(){
 		int number = getBuddyNumber();
 		switch(number){
 		case 0:
-			return "俺は" + getName() + "だ！あんたの名前は？";
+			return "教官は、この俺" + getName() + "だ！あんたの名前は？";
 		case 1:
-			return "ワシは" + getName() + "じゃ。お前さんは？";
+			return "教官は、このワシ" + getName() + "じゃ。お前さんは？";
 		case 2:
-			return "わたくしは" + getName() + "ですわ。あなた様は？";
+			return "教官は、わたくし" + getName() + "ですわ。あなた様は？";
 		case 3:
-			return "僕の名前は" + getName() + "です。君の名前は？";
+			return "教官は、僕" + getName() + "です。君の名前は？";
 		case 4:
-			return "私の名前は" + getName() + "よ。あなたは？";
+			return "教官は、私" + getName() + "よ。あなたは？";
 		default:
 			return "…" + getName()+"だ…";
 		}
