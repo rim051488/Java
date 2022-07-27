648d774dbd51101ab4e1c4c75b05cf6c30d322a6
rim051488 <sin.naga71@outlook.jp>
2022-07-27 15:19:59 +0900

    a

--- Ex06/SecureMyBuddy.java
+++ Ex06/SecureMyBuddy.java
@@ -8,7 +8,7 @@ public class SecureMyBuddy extends MyBuddy{
 		setPassword();
 	}
 	public void setPassword(){
-		set
+		myPassword = passwords[getBuddyNumber()];
 	}
 	public void setPassword(String password){
 		myPassword = password;
@@ -20,18 +20,18 @@ public class SecureMyBuddy extends MyBuddy{
 		return "不明";
 	}
 	public boolean verify(String password){
-
-
+		check = myPassword.equals(password);
+		return verify();
 	}
 	public boolean verify(){
 		return check;
 	}
 	@Override
 	public String getName(){
-
-
-
-
-
+		if (check){
+			return "俺の名前は" + super.getName() + "だっ！！";
+		}else{
+			return "俺の名前は教えられん！！";
+		}
 	}
 }
